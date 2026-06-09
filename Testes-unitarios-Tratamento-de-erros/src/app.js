e3e0878a7c683182c6068c7de0d79b271f8f4254
Guilherme <guilhermeb7810@gmail.com>
2026-06-09 14:02:33 -0300

    att cors app

--- Testes-unitarios-Tratamento-de-erros/src/app.js
+++ Testes-unitarios-Tratamento-de-erros/src/app.js
@@ -1,13 +1,12 @@
 import express from 'express';
+import cors from "cors";
 import morgan from 'morgan';
 import produtosRoutes from './routes/produtos.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import cors from "cors";
-
-app.use(cors());
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
